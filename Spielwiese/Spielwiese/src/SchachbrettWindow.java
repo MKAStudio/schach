b5559336e959751af3ba2b95eafe66064940666b
MKAStudio <137155056+MKAStudio@users.noreply.github.com>
2024-01-08 17:23:17 +0100

    layout optimization: resizable

--- Spielwiese/Spielwiese/src/SchachbrettWindow.java
+++ Spielwiese/Spielwiese/src/SchachbrettWindow.java
@@ -14,6 +14,15 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JMenu;
+import javax.swing.Box;
+import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import javax.swing.border.MatteBorder;
+import java.awt.Color;
+import java.awt.Window.Type;
+import net.miginfocom.swing.MigLayout;
 
 public class SchachbrettWindow {
 
@@ -52,49 +61,49 @@ public class SchachbrettWindow {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		schachbrett = new SchachbrettGUI(frame, 20, frame.getWidth(), 100, frame.getHeight());
-		schachbrett.setBounds(10, 28, 224, 224);
-        schachbrett.setMinimumSize(new Dimension((int)(frame.getWidth()/5*4), (int)(frame.getHeight()/5*4)));
-        frame.getContentPane().add(schachbrett);
+        frame.getContentPane().setLayout(new MigLayout("", "[320.00px,grow,trailing][100px:22.02%:250px,right]", "[20px][6px][23px][6px][20px][78.00px,grow][14px]"));
         
-        txtFieldMove = new JTextField();
-        txtFieldMove.setBounds(298, 28, 128, 20);
-        frame.getContentPane().add(txtFieldMove);
-        txtFieldMove.setColumns(10);
-        
-        JLabel lblNewLabel = new JLabel("Zug:");
-        lblNewLabel.setBounds(244, 28, 29, 20);
-        frame.getContentPane().add(lblNewLabel);
-        
-        JButton btnDoMove = new JButton("Do Move");
-        btnDoMove.setBounds(298, 59, 91, 23);
-        btnDoMove.addActionListener(new BtnDoMoveListener());
-        frame.getContentPane().add(btnDoMove);
-        
-        JLabel lblZug = new JLabel("waiting for task");
-        lblZug.setBounds(244, 238, 182, 14);
-        frame.getContentPane().add(lblZug);
+        schachbrett = new SchachbrettGUI(frame, 20, frame.getWidth(), 100, frame.getHeight());
+        frame.getContentPane().add(schachbrett, "cell 0 0 1 6,grow");
+        schachbrett.setMinimumSize(new Dimension(120, 120));
         
         JButton btnReset = new JButton("Reset Board");
-        btnReset.addActionListener(new BtnResetListener());
-        btnReset.setBounds(298, 204, 91, 23);
-        frame.getContentPane().add(btnReset);
-        
-        JLabel lblFeld = new JLabel("Feld:");
-        lblFeld.setBounds(244, 93, 29, 20);
-        frame.getContentPane().add(lblFeld);
-        
-        txtFieldField = new JTextField();
-        txtFieldField.setBounds(298, 93, 128, 20);
-        frame.getContentPane().add(txtFieldField);
-        txtFieldField.setColumns(10);
+        frame.getContentPane().add(btnReset, "cell 1 4,growx,gapx 10,aligny top");
         
         JButton btnShowMoves = new JButton("Show Moves");
+        frame.getContentPane().add(btnShowMoves, "cell 1 3,growx,gapx 10,aligny top");
+        
+        Box horizontalBox_2 = Box.createHorizontalBox();
+        schachbrett.setLayout(new SpringLayout());
+        frame.getContentPane().add(horizontalBox_2, "cell 1 0,growx,gapx 10,aligny top");
+        
+        JLabel lblFeld = new JLabel("Feld:");
+        horizontalBox_2.add(lblFeld);
+        
+        txtFieldField = new JTextField();
+        horizontalBox_2.add(txtFieldField);
+        txtFieldField.setColumns(10);
+        
+        Box horizontalBox_1 = Box.createHorizontalBox();
+        frame.getContentPane().add(horizontalBox_1, "cell 1 2,growx,gapx 10,aligny top");
+        
+        JLabel lblNewLabel = new JLabel("Zug:");
+        horizontalBox_1.add(lblNewLabel);
+        
+        txtFieldMove = new JTextField();
+        horizontalBox_1.add(txtFieldMove);
+        txtFieldMove.setColumns(10);
+        
+        JButton btnDoMove = new JButton("Do Move");
+        frame.getContentPane().add(btnDoMove, "cell 1 1,growx,gapx 10,aligny top");
+        
+        JLabel lblZug = new JLabel("waiting for task");
+        lblZug.setHorizontalTextPosition(SwingConstants.RIGHT);
+        lblZug.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        frame.getContentPane().add(lblZug, "cell 0 6 2 1,alignx center,aligny baseline");
+        btnDoMove.addActionListener(new BtnDoMoveListener());
         btnShowMoves.addActionListener(new BtnShowMovesListener());
-        btnShowMoves.setBounds(298, 124, 91, 23);
-        frame.getContentPane().add(btnShowMoves);
+        btnReset.addActionListener(new BtnResetListener());
 	
 	}
 	private static void addPopup(Component component, final JPopupMenu popup) {
